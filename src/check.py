f09c3ae94579c4b552ebb585f4e424b082ebf881
koyasushunpei <koyasu@comp.sd.keio.ac.jp>
2018-07-19 00:22:36 +0900

    [add] function for checking if tool exist in the tool list

---
+++ src/check.py
@@ -0,0 +1,17 @@
+import csv
+import src.output as output
+
+def check(tool_name, flag):
+    file = open("data/list.csv", "r")
+    reader = csv.reader(file)
+    header = next(reader)
+    file_exist = False
+    for row in reader:
+        if(row[0] == tool_name):
+            if(flag):
+                output.output(row, True) # full information
+            file_exist = True
+            break
+    if not(file_exist):
+        print("Can't find '{0}'".format(tool_name))
+        exit(0)
